f5f10914e691daf6735e6ae4c7d7095a1acf62ac
alt2cl <aldodiaz1983@gmail.com>
2020-07-29 17:37:26 -0400

    imagen retina

--- src/components/cards/CardBnr.jsx
+++ src/components/cards/CardBnr.jsx
@@ -1,13 +1,26 @@
 import React, { Fragment } from 'react'
 import iconArrow from './../../assets/img/icons/arrow-right.svg'
 
+import bnr from './../../assets/img/demo/destacados/bg_2020-07-03/bg.jpg'
+import bnrx2 from './../../assets/img/demo/destacados/bg_2020-07-03/bg@2x.jpg'
+import bnrx3 from './../../assets/img/demo/destacados/bg_2020-07-03/bg@3x.jpg'
+
 const CardBnr = (props) => {
+    let img = props.image;
+    let extension = img.substr(-4);
+    let arrtext= img.split(".");
+    let img2x = arrtext[0]+ '@2x' + extension;
+    //let img3x = arrtext[0]+ '@3x' + extension;
     return (
         <Fragment>
             <div className="bt_card-brand">
                 <a rel="noopener noreferrer" href={props.link} target="_blank" className="bt_brand-img">
                     <span className="lazyload-wrapper">
-                        <img alt="Despacho gratis" src={props.image}/>
+                        <picture>
+                            <source srcset={ img2x } media="(max-width: 768px)"/>
+                            <source srcset={ img } />
+                            <img src={ img } alt={props.name}/>
+                        </picture>
                     </span>
                 </a>
                 <a rel="noopener noreferrer" href={props.link} target="_blank" className="bt_link-btn-bullet d-flex justify-content-between">
